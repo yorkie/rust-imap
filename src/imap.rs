5154eca590c148f2987f105d830923da3416c645
Yazhong Liu <yorkiefixer@gmail.com>
2014-07-06 21:24:03 +0800

    add response

--- src/imap.rs
+++ src/imap.rs
@@ -4,12 +4,14 @@
 
 extern crate openssl;
 extern crate collections;
+extern crate core;
 
 use std::io::net::ip::SocketAddr;
 use std::io::net::tcp::TcpStream;
 use std::io::IoResult;
 use openssl::ssl::{SslStream, SslContext, Sslv23};
 use collections::string::String;
+use core::char;
 
 pub enum NetworkStream {
   NormalStream(TcpStream),
@@ -99,8 +101,57 @@ impl IMAPStream {
 
 }
 
+//
+// Parsing Response
+//
+
+struct IMAPResponse {
+  buffer: String,
+  lines: Vec<IMAPLine>,
+  tagged: bool,
+}
+
+impl IMAPResponse {
+
+  #[inline]
+  fn new() -> IMAPResponse {
+    IMAPResponse {
+      buffer: String::new(),
+      lines: Vec::new(),
+      tagged: false,
+    }
+  }
+
+  #[inline]
+  fn add_line(&mut self, line: IMAPLine) {
+    self.lines.push(line);
+    // self.buffer.append(line.raw);
+  }
+
+}
+
+struct IMAPLine {
+  tagged: bool,
+  raw: String,
+}
+
+impl IMAPLine {
+  fn new(bufs: String) -> IMAPLine {
+    let mut line = IMAPLine { tagged: false, raw: bufs };
+    let mut cursor = 0i;
+    while line.raw.len() > 0 {
+      if cursor == 0 {
+        line.tagged = line.raw.shift_char().unwrap() != '*';
+      }
+      cursor += 1;
+    }
+    return line;
+  }
+}
+
 #[inline]
 fn read_response(stream: &mut TcpStream) -> Result<String, Vec<u8>> {
+  let mut response = IMAPResponse::new();
   let mut bufs: Vec<u8> = Vec::new();
   let mut tryClose = false;
   loop {
@@ -108,19 +159,17 @@ fn read_response(stream: &mut TcpStream) -> Result<String, Vec<u8>> {
     stream.read(buf);
     bufs.push(buf[0]);
 
-    // check CLRL, if yes then break
+    // check CLRL firstly, if yes then break
     if tryClose && buf[0] == 0x0a {
+      match String::from_utf8(bufs) {
+        Ok(res) => response.add_line(IMAPLine::new(res)),
+        Err(e) => return Err(e),
+      }
       break;
     }
     tryClose = buf[0] == 0x0d;
   }
-  
-  match String::from_utf8(bufs) {
-    Ok(res) => {
-      return Ok(res);
-    },
-    Err(vec) => Err(vec),
-  }
+  Ok(String::new())
 }
 
 #[test]
